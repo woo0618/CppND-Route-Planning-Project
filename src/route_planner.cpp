67243bd415ab3a18eee2a77c0d8295ab967b69a9
woo0618 <37446787+woo0618@users.noreply.github.com>
2020-01-09 20:21:48 -0500

    update route_planner
    1. ConstructFinalPath function iteratively follows the parent nodes until start node
    2. update AStarSearch function

--- src/route_planner.cpp
+++ src/route_planner.cpp
@@ -43,38 +43,33 @@ RouteModel::Node *RoutePlanner::NextNode() {
     return node;
 }
 
-
-// TODO 6: Complete the ConstructFinalPath method to return the final path found from your A* search.
-// Tips:
-// - This method should take the current (final) node as an argument and iteratively follow the 
-//   chain of parents of nodes until the starting node is found.
-// - For each node in the chain, add the distance from the node to its parent to the distance variable.
-// - The returned vector should be in the correct order: the start node should be the first element
-//   of the vector, the end node should be the last element.
-
 std::vector<RouteModel::Node> RoutePlanner::ConstructFinalPath(RouteModel::Node *current_node) {
     // Create path_found vector
     distance = 0.0f;
     std::vector<RouteModel::Node> path_found;
-
-    // TODO: Implement your solution here.
-
+    
+    auto next = current_node->parent;
+    while (current_node != start_node) {
+        path_found.push_back(*current_node);
+        distance += next->distance(*current_node);
+        current_node = next;
+        next = current_node->parent;
+    }
+    
+    path_found.push_back(*start_node);
     distance *= m_Model.MetricScale(); // Multiply the distance by the scale of the map to get meters.
+    std::reverse(path_found.begin(), path_found.end());
     return path_found;
-
 }
-
-
-// TODO 7: Write the A* Search algorithm here.
-// Tips:
-// - Use the AddNeighbors method to add all of the neighbors of the current node to the open_list.
-// - Use the NextNode() method to sort the open_list and return the next node.
-// - When the search has reached the end_node, use the ConstructFinalPath method to return the final path that was found.
-// - Store the final path in the m_Model.path attribute before the method exits. This path will then be displayed on the map tile.
 
 void RoutePlanner::AStarSearch() {
     RouteModel::Node *current_node = nullptr;
-
-    // TODO: Implement your solution here.
-
+    current_node = start_node;
+    start_node->visited = true;
+    
+    while (current_node != end_node) {
+        AddNeighbors(current_node);
+        current_node = NextNode();
+    }
+    m_Model.path = ConstructFinalPath(end_node);
 }
